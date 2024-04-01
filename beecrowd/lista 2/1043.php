32e3318198022835e8319cb442148dca684f7004
Dejair Alves Neto <108379720+Netoswag@users.noreply.github.com>
2024-04-01 14:25:20 -0300

    Update 1043.php

--- beecrowd/lista 2/1043.php
+++ beecrowd/lista 2/1043.php
@@ -4,3 +4,4 @@ if (($A + $B) > $C && ($A + $C) > $B && ($C + $B) > $A)
     echo "Perimetro = " . number_format(($A + $B + $C), 1, '.', '') . "\n";
 else
     echo "Area = " . number_format(((($A + $B) * $C) / 2), 1, '.', '') . "\n";
+?>
